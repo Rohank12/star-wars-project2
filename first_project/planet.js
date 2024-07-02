2f35f5262604959a3f7ab147b45f7eca100237dc
Alice Smith <alice@webage.com>
2024-07-02 11:18:38 -0400

    added first project files

---
+++ first_project/planet.js
@@ -0,0 +1,74 @@
+let nameH1;
+let climateSpan;
+let terrainSpan;
+let popSpan;
+let characterUl;
+let filmsDiv;
+const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
+let storagePlanets = JSON.parse(localStorage.getItem("storagePlanets"))
+if (!storagePlanets) {
+    storagePlanets = new Array()
+}
+console.log(storagePlanets)
+// Runs on page load
+addEventListener('DOMContentLoaded', () => {
+  nameH1 = document.querySelector('h1#name');
+  climateSpan = document.querySelector('span#climate');
+  terrainSpan = document.querySelector('span#terrain');
+  popSpan = document.querySelector('span#population');
+  characterUl = document.querySelector('#characters>ul');
+  filmsUl = document.querySelector('#films>ul');
+  const sp = new URLSearchParams(window.location.search)
+  const id = sp.get('id')
+  getPlanet(id)
+});
+
+async function getPlanet(id) {
+  let planet;
+  try {
+    planet = await fetchPlanet(id)
+    storagePlanets[id] = planet.name
+    planet.characters = await fetchCharacters(planet)
+    planet.films = await fetchFilms(planet)
+  }
+  catch (ex) {
+    console.error(`Error reading planet ${id} data.`, ex.message);
+  }
+  renderPlanet(planet);
+
+}
+async function fetchPlanet(id) {
+  let planetUrl = `${baseUrl}/planets/${id}`;
+  return await fetch(planetUrl)
+    .then(res => res.json())
+}
+
+async function fetchCharacters(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/characters`;
+  const characters = await fetch(url)
+    .then(res => res.json())
+  return characters;
+}
+
+async function fetchFilms(planet) {
+  const url = `${baseUrl}/planets/${planet?.id}/films`;
+  const films = await fetch(url)
+    .then(res => res.json())
+  return films;
+}
+
+const renderPlanet = planet => {
+  document.title = `SWAPI - ${planet?.name}`;  // Just to make the browser tab say the name
+  localStorage.setItem("planetStorage", JSON.stringify(planetStorage));
+  nameH1.textContent = planet?.name;
+  climateSpan.textContent = planet?.climate;
+  terrainSpan.textContent = planet?.terrain;
+  popSpan.textContent = planet?.population;
+  
+  const charLis = planet?.characters.sort((a, b) => a.name > b.name ? 1 : -1).map(character => `<li><a href="/character.html?id=${character.id}">${character.name}</li>`);
+  characterUl.innerHTML = charLis.join("");
+  
+  const filmsLis = planet?.films?.sort((a, b) => a.title > b.title ? 1 : -1).map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+  filmsUl.innerHTML = filmsLis.join("");
+}
