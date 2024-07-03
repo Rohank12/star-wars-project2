5b658b8fbe89bc3244a78e2e2d5d79875ecfd1e0
Guillermo <pulparindo1414@gmail.com>
2024-07-03 11:28:59 -0400

    Planets

--- react-star-wars/src/App.jsx
+++ react-star-wars/src/App.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from '../components/Home'
+import Planets from "../components/Planets";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
 import './App.css'
 
 function App() {
@@ -19,34 +24,31 @@ function App() {
             console.error('Error fetching characters:', error);
         }
     };
-
     fetchData();
   }, []);
 
-  console.log(data)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <meta charSet="UTF-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Star Wars Characters</title>
+      <link rel="stylesheet" href="App.css" />
+      <div id="search-div">
+        <h1>Star Wars Universe Lookup</h1>
+        <label htmlFor="searchString">
+          Who you looking for?{" "}
+          <span className="small">(Regular expressions are cool here)</span>
+        </label>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div id="charactersList">
+        <Routes>
+          <Route path="/" element={<Home data={data} />} />
+          <Route path="/planets/:id" element={<Planets/>} />
+        </Routes>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        <Home data={data} />
-      </div>
+    </Router>
     </>
   )
 }
